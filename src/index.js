c6715a9ed5eeb97ff5a6362e48c640ca8af3ed43
BalaPriyanB <117967016+BalaPriyanB@users.noreply.github.com>
2024-05-17 20:46:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import store from "./store";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: false,
+      refetchOnWindowFocus: true,
     },
   },
 });
