4c293d77f0306e6426f382309986e2a56ca9c3f1
BalaPriyanB <117967016+BalaPriyanB@users.noreply.github.com>
2024-05-18 21:45:19 +0530

    Update ArticleCard.jsx

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -30,39 +30,6 @@ const ArticleCard = ({ post, className }) => {
             {post.caption}
           </p>
         </Link>
-        <div className="flex justify-between flex-nowrap items-center mt-6">
-          <div className="flex items-center gap-x-2 md:gap-x-2.5">
-            <img
-              src={
-                post.user.avatar
-                  ? stables.UPLOAD_FOLDER_BASE_URL + post.user.avatar
-                  : images.userImage
-              }
-              alt="post profile"
-              className="w-9 h-9 md:w-10 md:h-10 rounded-full"
-            />
-            <div className="flex flex-col">
-              <h4 className="font-bold italic text-dark-soft text-sm md:text-base">
-                {post.user.name}
-              </h4>
-              <div className="flex items-center gap-x-2">
-                <span
-                  className={`${
-                    post.user.verified ? "bg-[#36B37E]" : "bg-red-500"
-                  } w-fit bg-opacity-20 p-1.5 rounded-full`}
-                >
-                  {post.user.verified ? (
-                    <BsCheckLg className="w-1.5 h-1.5 text-[#36B37E]" />
-                  ) : (
-                    <AiOutlineClose className="w-1.5 h-1.5 text-red-500" />
-                  )}
-                </span>
-                <span className="italic text-dark-light text-xs md:text-sm">
-                  {post.user.verified ? "Verified" : "Unverified"} writer
-                </span>
-              </div>
-            </div>
-          </div>
           <span className="font-bold text-dark-light italic text-sm md:text-base">
             {new Date(post.createdAt).getDate()}{" "}
             {new Date(post.createdAt).toLocaleString("default", {
