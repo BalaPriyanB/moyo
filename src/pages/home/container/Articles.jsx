3134f3138172512337daf19072c915e94acef2cb
BalaPriyanB <117967016+BalaPriyanB@users.noreply.github.com>
2024-06-06 22:12:15 +0530

    Update Articles.jsx

--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -19,13 +19,13 @@ const Articles = () => {
   });
 
   return (
-    <section className="flex flex-col container mx-auto px-5 py-10">
+    <section className="flex flex-col container mx-auto px-5 py-10 bg-dark-hard text-dark-soft">
       <div className="flex flex-wrap md:gap-x-5 gap-y-5 pb-10">
         {isLoading ? (
           [...Array(3)].map((_, index) => (
             <ArticleCardSkeleton
               key={index}
-              className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)] dark"
+              className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)] bg-dark-hard text-dark-soft"
             />
           ))
         ) : isError ? (
@@ -39,14 +39,14 @@ const Articles = () => {
             <ArticleCard
               key={post._id}
               post={post}
-              className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)] dark"
+              className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)] bg-dark-hard text-dark-soft"
             />
           ))
         )}
       </div>
       <Link
         to="/blog"
-        className="mx-auto flex items-center gap-x-2 font-bold text-primary border-2 border-primary px-6 py-3 rounded-lg dark"
+        className="mx-auto flex items-center gap-x-2 font-bold text-primary border-2 border-primary px-6 py-3 rounded-lg bg-dark-hard text-dark-soft"
       >
         <span>More articles</span>
         <FaArrowRight className="w-3 h-3" />
