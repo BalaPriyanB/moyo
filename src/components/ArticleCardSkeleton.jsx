99115bd526bd0b214f4e76af2e3d072412e07ef0
BalaPriyanB <117967016+BalaPriyanB@users.noreply.github.com>
2024-07-07 22:59:17 +0530

    Update ArticleCardSkeleton.jsx

--- src/components/ArticleCardSkeleton.jsx
+++ src/components/ArticleCardSkeleton.jsx
@@ -1,15 +1,15 @@
 const ArticleCardSkeleton = ({ className }) => {
   return (
     <div
-      className={`rounded-xl overflow-hidden shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] ${className} animate-pulse bg-dark-bg`}
+      className={`rounded-xl overflow-hidden shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] ${className} animate-pulse bg-dark-soft`}
     >
       {/* image */}
-      <div className="w-full aspect-video bg-dark-slate" />
+      <div className="w-full aspect-video bg-dark-soft" />
       <div className="p-5">
         {/* title */}
-        <div className="w-56 h-2 mt-4 bg-dark-slate rounded-lg" />
+        <div className="w-56 h-2 mt-4 bg-dark-soft rounded-lg" />
         {/* caption */}
-        <div className="w-24 h-2 mt-4 bg-dark-slate rounded-lg" />
+        <div className="w-24 h-2 mt-4 bg-dark-soft rounded-lg" />
       </div>
     </div>
   );
