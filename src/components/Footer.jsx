b126ce67e77af5c4cb9549eb93ae73ce344948af
BalaPriyanB <117967016+BalaPriyanB@users.noreply.github.com>
2024-07-07 23:33:19 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,9 +13,8 @@ import { images } from "../constants";
 const Footer = () => {
   return (
     <section className="bg-dark-hard">
-    
+    <hr className="w-full border-t border-white mt-4" />
       <footer className="container mx-auto grid grid-cols-10 px-5 py-10 gap-y-10 gap-x-5 md:pt-20 md:grid-cols-12 lg:grid-cols-10 lg:gap-x-10">
-      <hr className="w-full border-t border-white mt-4" />
         <div className="col-span-5 md:col-span-4 lg:col-span-2">
           <h3 className="text-dark-light font-bold md:text-lg">Movies</h3>
           <ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
