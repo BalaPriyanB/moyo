3cb7e8970af6e9bed7a6fb31cc8c27fa459e048f
BalaPriyanB <117967016+BalaPriyanB@users.noreply.github.com>
2024-07-08 22:45:43 +0530

    Update Articles.jsx

--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -1,31 +1,27 @@
 import React from "react";
-import { FaArrowRight } from "react-icons/fa";
-import ArticleCard from "../../../components/ArticleCard";
-import { useQuery } from "@tanstack/react-query";
-import { getAllPosts } from "../../../services/index/posts";
-import { toast } from "react-hot-toast";
-import ArticleCardSkeleton from "../../../components/ArticleCardSkeleton";
-import ErrorMessage from "../../../components/ErrorMessage";
 import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa";
+import ArticleCard from "./ArticleCard";
+import ArticleCardSkeleton from "./ArticleCardSkeleton";
+import ErrorMessage from "../../components/ErrorMessage";
+import { useQuery } from "@tanstack/react-query";
+import { getAllPosts } from "../../services/index/posts";
+import { images } from "../../constants";
 
-const Articles = () => {
+const ArticleList = () => {
   const { data, isLoading, isError } = useQuery({
-    queryFn: () => getAllPosts("", 1, 6),
-    queryKey: ["posts"],
-    onError: (error) => {
-      toast.error(error.message);
-      console.log(error);
-    },
+    queryFn: getAllPosts,
+    queryKey: "posts",
   });
 
   return (
-    <section className="flex flex-col container mx-auto px-5 py-10 bg-dark-hard text-white">
-      <div className="flex flex-wrap md:gap-x-5 gap-y-5 pb-10">
+    <section className="container mx-auto px-5 py-10 bg-dark-hard text-white">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 pb-10">
         {isLoading ? (
-          [...Array(3)].map((_, index) => (
+          [...Array(5)].map((_, index) => (
             <ArticleCardSkeleton
               key={index}
-              className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)] dark"
+              className="w-full md:w-[calc(20%-20px)] lg:w-[calc(20%-21px)] dark"
             />
           ))
         ) : isError ? (
@@ -39,14 +35,14 @@ const Articles = () => {
             <ArticleCard
               key={post._id}
               post={post}
-              className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)] dark text-white"
+              className="w-full md:w-[calc(20%-20px)] lg:w-[calc(20%-21px)] dark text-white"
             />
           ))
         )}
       </div>
       <Link
         to="/blog"
-        className="mx-auto flex items-center gap-x-2 font-bold border-2 border-primary px-6 py-3 rounded-lg dark text-white"
+        className="mx-auto flex items-center gap-2 font-bold border-2 border-primary px-6 py-3 rounded-lg dark text-white"
       >
         <span>More articles</span>
         <FaArrowRight className="w-3 h-3" />
@@ -55,4 +51,4 @@ const Articles = () => {
   );
 };
 
-export default Articles;
+export default ArticleList;
