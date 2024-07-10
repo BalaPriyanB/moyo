079fa4476577d49e6999c0d7b03f27cd16d67ef3
BalaPriyanB <117967016+BalaPriyanB@users.noreply.github.com>
2024-07-10 21:09:07 +0530

    Update Articles.jsx

--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -46,8 +46,8 @@ const Articles = () => {
       </div>
       <Link
         to="/blog"
-        className="grid items-center gap-4 font-bold border-2 border-primary rounded-lg dark text-white"
-      >
+        className="mx-auto flex items-center gap-2 font-bold border-2 border-primary px-6 py-3 rounded-lg dark text-white"
+        >
         <span>More articles</span>
         <FaArrowRight className="w-3 h-3" />
       </Link>
