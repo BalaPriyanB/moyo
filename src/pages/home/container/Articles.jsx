a5fd491fe65c7a21c3557a5222141c91dc69cce4
BalaPriyanB <117967016+BalaPriyanB@users.noreply.github.com>
2024-07-10 21:17:23 +0530

    Update Articles.jsx

--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -19,13 +19,13 @@ const Articles = () => {
   });
 
   return (
-    <section className="grid grid-cols-2 container bg-dark-hard text-white">
-      <div className="grid grid-cols-2 lg:grid-cols-5 gap-4 pb-10">
+    <section className="flex flex-col container mx-auto px-5 py-10 bg-dark-hard text-white">
+      <div className="flex flex-wrap md:gap-x-5 gap-y-5 pb-10">
         {isLoading ? (
           [...Array(3)].map((_, index) => (
             <ArticleCardSkeleton
               key={index}
-              className="w-full dark"
+              className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)] dark"
             />
           ))
         ) : isError ? (
@@ -39,15 +39,15 @@ const Articles = () => {
             <ArticleCard
               key={post._id}
               post={post}
-              className="w-full dark text-white"
+              className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)] dark text-white"
             />
           ))
         )}
       </div>
       <Link
         to="/blog"
-        className="mx-auto flex items-center gap-2 font-bold border-2 border-primary px-6 py-3 rounded-lg dark text-white"
-        >
+        className="mx-auto flex items-center gap-x-2 font-bold border-2 border-primary px-6 py-3 rounded-lg dark text-white"
+      >
         <span>More articles</span>
         <FaArrowRight className="w-3 h-3" />
       </Link>
