a2f4c8a7757dda081a90126dbfe820327c1666ce
BalaPriyanB <117967016+BalaPriyanB@users.noreply.github.com>
2024-07-11 21:09:57 +0530

    Create postTags.js

---
+++ src/services/index/postTags.js
@@ -0,0 +1,90 @@
+import axios from "axios";
+const base = "https://moyo-mhe5.onrender.com";
+
+
+export const getAllTags = async (
+  searchKeyword = "",
+  page = 1,
+  limit = 10
+) => {
+  try {
+    const { data, headers } = await axios.get(
+      `${base}/api/post-tags?searchKeyword=${searchKeyword}&page=${page}&limit=${limit}`
+    );
+    return { data, headers };
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
+
+export const deleteTagy = async ({ slug, token }) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const { data } = await axios.delete(`${base}/api/post-tags/${slug}`, config);
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
+
+export const createTagy = async ({ token, title }) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const { data } = await axios.post(
+      `${base}/api/post-tags`,
+      { title },
+      config
+    );
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
+
+export const updateTagy = async ({ token, title, slug }) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const { data } = await axios.put(
+      `${base}/api/post-tags/${slug}`,
+      { title },
+      config
+    );
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
+
+export const getSingleTagy = async ({ slug }) => {
+  try {
+    const { data } = await axios.get(`${base}/api/post-tags/${slug}`);
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
